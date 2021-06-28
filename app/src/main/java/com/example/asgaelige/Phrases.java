627f277ef97fd4d3722dc2c453c14f1bf2006835
CaolanCode <cmacmahon3@gmail.com>
2021-06-28 07:48:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/asgaelige/Phrases.java
@@ -0,0 +1,16 @@
+package com.example.asgaelige;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Phrases extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+
+        this.setTitle("Phrases/Frásaí");
+    }
+}
