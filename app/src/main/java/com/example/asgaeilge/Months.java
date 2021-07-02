1fe74d2b2cdf2914ff3157cc7b44d8525e358c78
CaolanCode <cmacmahon3@gmail.com>
2021-07-02 09:50:25 +0100

    Added a color scheme

--- app/src/main/java/com/example/asgaeilge/Months.java
+++ app/src/main/java/com/example/asgaeilge/Months.java
@@ -2,15 +2,14 @@ package com.example.asgaeilge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Months extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_months);
-
-        this.setTitle(getString(R.string.míonna));
     }
 }
